f69c7b09b938b2af6838fc0c6919b75f654c34c5
Mikito Takada <firstname.surname@gmail.com>
2011-08-26 21:25:42 -0700

    Got it to buildgit add -Agit add -A

--- hello.cc
+++ hello.cc
@@ -6,6 +6,14 @@
 #include <v8.h>
 #include <node.h>
 
+#include <X11/Xlib.h>
+#include <assert.h>   // I include this to test return values the lazy way
+#include <unistd.h>   // So we got the profile for 10 seconds
+#define NIL (0)       // A name for the void pointer
+
+#define WIDTH 800
+#define HEIGHT 600
+
 using namespace node;
 using namespace v8;
 
@@ -63,6 +71,44 @@ public:
     HelloWorld* hw = ObjectWrap::Unwrap<HelloWorld>(args.This());
     // use hello world
     hw->m_count++;
+
+
+     Display *dpy;
+     Window wnd;
+
+     dpy = XOpenDisplay(NIL);
+
+     int blackColor = BlackPixel(dpy, DefaultScreen(dpy));
+     int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
+
+     wnd = XCreateSimpleWindow(dpy,
+                          DefaultRootWindow(dpy), 0, 0, 
+                 WIDTH, HEIGHT, 0, 
+                 blackColor, blackColor);
+     // We want to get MapNotify events
+
+     XSelectInput(dpy, wnd, StructureNotifyMask);
+     XMapWindow(dpy, wnd);
+     
+     GC gc = XCreateGC(dpy, wnd, 0, NIL);
+     XSetForeground(dpy, gc, whiteColor);
+
+     
+        // Wait for the MapNotify event
+
+        for(;;) {
+         XEvent e;
+         XNextEvent(dpy, &e);
+         if (e.type == MapNotify)
+          break;
+        }
+        XDrawLine(dpy, wnd, gc, 10, 60, 180, 20);
+     
+     
+     XFlush(dpy);
+        sleep(10);
+
+
     // create and return a new string
     Local<String> result = String::New("Hello World");
     return scope.Close(result);
--- hello.js
+++ hello.js
@@ -2,3 +2,5 @@ var HelloWorld = require('./build/default/helloworld.node').HelloWorld;
 
 var obj = new HelloWorld();
 console.log(obj.hello());
+
+setTimeout(function() {}, 10000);
