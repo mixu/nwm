f8921865b6d34cce06f9a5faaf4607b3d2118311
Mikito Takada <firstname.surname@gmail.com>
2011-08-28 15:59:03 -0700

    Wrote a tween for circular motion.

--- nwm.js
+++ nwm.js
@@ -191,7 +191,62 @@ NWM.prototype.random = function() {
   });
 };
 
+NWM.prototype.globalSmall = function() {
+  var self = this;
+  var keys = Object.keys(this.windows);
+  keys.forEach(function(id, index) {
+    self.resize(id, 200, 200);    
+  });  
+};
 
+var tweens = [];
+
+NWM.prototype.tween = function(id) {
+  var self = this;
+  var radius = 200;
+  var circle_x = Math.floor(Math.random()*(self.screen.width));
+  var circle_y = Math.floor(Math.random()*(self.screen.height));
+  if(circle_x+radius*2 > self.screen.width) {
+    circle_x -= radius*2;
+  }
+  if(circle_y+radius*2 > self.screen.height) {
+    circle_y -= radius*2;
+  }
+  if(circle_x-radius*2 < 0) {
+    circle_x += radius*2;
+  }
+  if(circle_y-radius*2 < 0) {
+    circle_y += radius*2;
+  }
+
+  function circularPath(index) {
+
+    var cx = circle_x;
+    var cy = circle_y;
+    var aStep = 3;   // 3 degrees per step
+    var theta = index * aStep;  // +ve angles are cw
+
+    var newX = cx + radius * Math.cos(theta * Math.PI / 180);
+    var newY = cy + radius * Math.sin(theta * Math.PI / 180);
+
+    // return an object defining state that can be understood by drawFn
+    return  {x: newX, y: newY};
+  }
+  var step = 0;
+  var result = setInterval(function() {
+    step++;
+    var pos = circularPath(step);
+    self.move(id, Math.floor(pos.x), Math.floor(pos.y));
+  }, 1000 / 60);
+  tweens.push({ id: id, interval: result} );
+};
+
+NWM.prototype.stop = function() {
+  for(var i = 0; i < tweens.length; i++) {
+    clearInterval(tweens[i]); 
+  }
+  tweens = [];
+};
 
 
 var nwm = new NWM();
