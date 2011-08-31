ed973718d3e1a132c31bf1ad9b9e7aa2663e8d7d
Mikito Takada <firstname.surname@gmail.com>
2011-08-31 03:53:45 -0700

    Add keysymdef - coding at SFO Intl

--- nwm.js
+++ nwm.js
@@ -1,7 +1,7 @@
 var repl = require('repl');
 var X11wm = require('./build/default/nwm.node').NodeWM;
 var child_process = require('child_process');
-
+var X11keysym = require('./keysymdef.js');
 
 var NWM = function() {
   this.windows = {};
@@ -69,11 +69,19 @@ NWM.prototype.start = function() {
   this.wm.on('keyPress', function(key) {
     // do something, e.g. launch a command
     var chr = String.fromCharCode(key.keysym);
-    console.log('keyPress', key, chr, '0'.charCodeAt(0), '9'.charCodeAt(0));
-    if( key.keysym > '0'.charCodeAt(0) && key.keysym < '9'.charCodeAt(0)) {
+    var keysym_name = '';
+    Object.keys(X11keysym).every(function(name) {
+      if(X11keysym[name] == key.keysym) {
+        keysym_name = name;
+        return false; // stop iteration
+      }
+      return true;
+    });
+    console.log('keyPress', key, chr, keysym_name);
+    if( key.keysym > X11keysym.XK_KP_0 && key.keysym < X11keysym.XK_KP_9) {
       self.go(chr); // jump to workspace
     }
-    if(key.keysym == 0xFF0D) {
+    if(key.keysym == X11keysym.XK_Return) {
       // enter pressed ...
       console.log('Enter key, start xterm');
       child_process.spawn('xterm', ['-lc'], { env: { 'DISPLAY': ':1' } });
