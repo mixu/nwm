305c279066e65ba8b5d9866b8b88102ed8dc69dd
Mikito Takada <firstname.lastname@gmail.com>
2011-12-02 20:22:39 -0800

    Forgot to export layouts.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   NWM: require('./nwm.js'),
   keysymdef: require('./lib/keysymdef.js'),
-  Xh: require('./lib/x.js')
+  Xh: require('./lib/x.js'),
+  layouts: require('./lib/layouts')
 };
